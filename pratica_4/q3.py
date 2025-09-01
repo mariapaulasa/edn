b5e6cc691db705d14965eb234b8696e98cf9e85e
Maria Paula <mariapaulasa@gmail.com>
2025-08-31 21:01:47 -0300

    pratica 4

---
+++ pratica_4/q3.py
@@ -0,0 +1,16 @@
+'''
+
+Crie um programa que verifique se uma senha é forte. Uma senha forte deve ter pelo menos 8 caracteres e conter pelo menos um número. O programa deve continuar pedindo senhas até que uma válida seja inserida ou o usuário digite 'sair'.
+
+'''
+
+while True:
+    senha = input("Digite uma senha (ou 'sair' para encerrar): ")
+    if senha.lower() == 'sair':
+        print("Encerrando o programa.")
+        break
+    if len(senha) >= 8 and any(char.isdigit() for char in senha):
+        print("Senha forte! Acesso concedido.")
+        break
+    else:
+        print("Senha fraca. Tente novamente.")
